59c3253fcf379ad62f8a5c1bc76c4c4d34b7a0e2
unknown <jaykishankharwar215@gmail.com>
2023-12-12 20:26:26 +0530

    Uploaded

--- A-3 Problems on Loops/Question_9.cpp
+++ A-3 Problems on Loops/Question_9.cpp
@@ -12,6 +12,7 @@ using namespace std;
 // Function Declaration.
 bool isPrime(int x);
 void GeneratePrimeNumber();
+int PreviousPrimeNo(int num);
 
 // Global Varieble Declaration
 int PrimeNumber = -1;
@@ -20,15 +21,36 @@ int lastPrime = PrimeNumber + 1;
 // Main Function start
 int main()
 {
-    GeneratePrimeNumber();
-    cout << PrimeNumber << endl;
-    GeneratePrimeNumber();
-    cout << PrimeNumber << endl;
-    GeneratePrimeNumber();
-    cout << PrimeNumber << endl;
-    GeneratePrimeNumber();
-    cout << PrimeNumber << endl;
-    // Return
+    // Take Number from user
+    int num;
+    cout << "Enter your Number : ";
+    cin >> num;
+
+    // if num is already prime then print massage and return
+    if (isPrime(num))
+    {
+        cout << endl
+             << "The number can't expressed as the sum of the prime number ";
+        return 0;
+    }
+
+    // Find the previous Prime Number to the Number
+    int PrePrimeNo = PreviousPrimeNo(num);
+    // find the second prime number which can be added to Previous Prime number to make the Orignal Number.
+    int secondNo = -1;
+    while (1)
+    {
+        GeneratePrimeNumber();
+        if (PrePrimeNo + lastPrime == num)
+        {
+            secondNo = lastPrime;
+            break;
+        }
+    }
+
+    // At End print following massage.
+    cout << "It Expressed as : " << PrePrimeNo << " + " << secondNo << endl;
+
     return 0;
 }
 
@@ -53,6 +75,21 @@ bool isPrime(int x)
 
     // All process is clear so return true.
     return true;
+}
+
+int PreviousPrimeNo(int num)
+{
+    int result = -1;
+    while (num > 0)
+    {
+        if (isPrime(num))
+        {
+            result = num;
+            break;
+        }
+        num--;
+    }
+    return result;
 }
 
 void GeneratePrimeNumber()
